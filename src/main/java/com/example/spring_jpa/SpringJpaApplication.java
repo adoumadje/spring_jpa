5503ef815ec4ca2d6a4cc8972103010cd4c2f99c
adoumadje <adoumadjecaleb@gmail.com>
2022-08-17 11:16:37 +0300

    last commit

--- src/main/java/com/example/spring_jpa/SpringJpaApplication.java
+++ src/main/java/com/example/spring_jpa/SpringJpaApplication.java
@@ -1,7 +1,9 @@
 package com.example.spring_jpa;
 
+import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
 
 @SpringBootApplication
 public class SpringJpaApplication {
@@ -10,4 +12,16 @@ public class SpringJpaApplication {
         SpringApplication.run(SpringJpaApplication.class, args);
     }
 
+    @Bean
+    CommandLineRunner commandLineRunner(StudentRepository studentRepository) {
+        return args -> {
+            Student coco = new Student(
+                    "Coco",
+                    "JONES",
+                    "coco.jones@fkmail.com",
+                    23
+            );
+            studentRepository.save(coco);
+        };
+    }
 }
